515c86b161dd6da156133d6728b0ad77599625e0
parv15 <mittalparv2000@gmail.com>
2023-03-11 01:49:02 +0530

    Teacher Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Section from './pages/Section';
+import Teacher from './pages/Teacher';
 
 
 const App = () => {
 
   return(
-    <Section/>
+    <>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Section/>} />
+      <Route path="/Teacher" element={<Teacher/>} />
+      </Routes>
+    </BrowserRouter>
+    </>
   )
   
 }
