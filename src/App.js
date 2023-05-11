7cf55dd53f91645276c497c7d1a32d6a2baba020
parv15 <mittalparv2000@gmail.com>
2023-05-11 11:12:54 +0530

    Register Teacher Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,Route, Routes } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import './App.css';
 import CreateTimeTable from './pages/Admin/CreateTimeTable/CreateTimeTable';
 import Student from './pages/Student/Student';
@@ -7,8 +7,8 @@ import Header from './components/Header/Header';
 import Teacher from './pages/Teacher/Teacher';
 import Login from './pages/Login/Login';
 import AdminFacultyVIew from './pages/Admin/AdminFacultyView/AdminFacultyView';
-import Register from "./pages/Register/Register"
-import ProfileUpdate from './pages/Admin/Teacher Profile Update/ProfileUpdate';
+import Register from "./pages/Admin/RegisterTeacher/Register"
+import ProfileUpdate from './pages/Admin/TeacherProfileUpdate/ProfileUpdate';
 
 
 const App = () => {
