7bc54e580f3306ae898dd009d078cef0211afcc3
parv15 <mittalparv2000@gmail.com>
2023-05-19 21:53:31 +0530

    Nav Fix

--- src/components/TeacherNav/TeacherNav.js
+++ src/components/TeacherNav/TeacherNav.js
@@ -1,48 +1,47 @@
-import React, { useState} from 'react';
-// import "./AdminNav.css";
-import LogoutBtn from '../Logout/LogoutBtn';
-import { useNavigate } from 'react-router-dom';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close'; 
+
+import React, { useState } from "react";
+
+import LogoutBtn from "../Logout/LogoutBtn";
+import { useNavigate } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 // import { Menu } from '@mui/icons-material';
 import "./TeacherNav.css"
 const TeacherNav = () => {
-  const [openNav,setOpenNav]=useState(true);
-  const navigate = useNavigate()
+  const [openNav, setOpenNav] = useState(true);
+  const navigate = useNavigate();
 
-  const openMenu=()=>{
-   
-    setOpenNav(false)
-    
-  }
-    
-   const closeMenu=()=>{
-    setOpenNav(true)
-   }
-  
- 
+  const openMenu = () => {
+    setOpenNav(false);
+  };
+
+  const closeMenu = () => {
+    setOpenNav(true);
+  };
+
   return (
     <>
-      <div className={openNav?'menubar':'hide'} onClick={openMenu}>
-        <MenuIcon/>
+      <div className={openNav ? "menubar" : "hide"} onClick={openMenu}>
+        <MenuIcon />
       </div>
-      <div className={openNav?'hide':'menubar'} onClick={closeMenu}>
-        <CloseIcon/>
+      <div className="AdminNavbar">
+        {/* {openNav?<CloseIcon sx={{margin:"3%",color:"white"}} onClick={closeMenu}/>:null} */}
+        <button style={{padding:"0.8rem 1rem"}} onClick={()=>navigate("/myprofile" )} >My Profile</button>
+          <button style={{padding:"0.5rem 1rem"}} onClick={()=>navigate("/teacher")}>View TimeTable</button>
+          <LogoutBtn/>
+       
       </div>
-      <div className='AdminNavbar' id='teachernav'>
-          {/* {openNav?<CloseIcon sx={{margin:"3%",color:"white"}} onClick={closeMenu}/>:null} */}
-          <button style={{padding:"0.8rem 1rem"}} onClick={()=>navigate("/myprofile" )} >My Profile</button>
+      <div className={openNav ? "hide" : "AdminNavbarMobile"}>
+        <div className={openNav ? "hide" : "menubar"} onClick={closeMenu}>
+          <CloseIcon sx={{color:"white"}}/>
+        </div>
+        {/* {openNav?<CloseIcon sx={{margin:"3%",color:"white"}} onClick={closeMenu}/>:null} */}
+        <button style={{padding:"0.8rem 1rem"}} onClick={()=>navigate("/myprofile" )} >My Profile</button>
           <button style={{padding:"0.5rem 1rem"}} onClick={()=>navigate("/teacher")}>View TimeTable</button>
           <LogoutBtn/>
       </div>
-      <div className={openNav?'hide':'AdminNavbarMobile'}>
-          {/* {openNav?<CloseIcon sx={{margin:"3%",color:"white"}} onClick={closeMenu}/>:null} */}
-          <button  onClick={()=>navigate("/myprofile" )} >My Profile</button>
-          <button  onClick={()=>navigate("/teacher")}>View TimeTable</button>
-          <LogoutBtn/>
-      </div>
     </>
-  )
-}
+  );
+};
 
-export default TeacherNav
+export default TeacherNav;
