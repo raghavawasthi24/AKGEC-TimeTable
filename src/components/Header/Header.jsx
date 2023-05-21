bf0eb10e98125764ff9e3e75458e413f3a4f6c12
parv15 <mittalparv2000@gmail.com>
2023-05-21 18:17:21 +0530

    Refresh Token

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,23 +5,21 @@ import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
 
-  const navigate = useNavigate()
-
-
-  console.log(localStorage.getItem('refreshToken'))
+ const navigate = useNavigate()
+ 
   useEffect(() => {
       setInterval(() => {
         if(localStorage.getItem('refreshToken')){
           const AuthStr = 'Bearer '.concat(localStorage.getItem("accessToken"))
           axios.defaults.headers.common['Authorization'] = AuthStr;
-        axios.post(`${process.env.REACT_APP_URL}/accounts/refresh-token`,)
+        axios.post(`${process.env.REACT_APP_URL}/accounts/refresh-token`,{token: localStorage.getItem('refreshToken')})
         .then((response)=>{
           (localStorage.setItem("accessToken",response.data.access))}
           )
         .catch((err)=>{
           if(err.response.status === 400 ){
             localStorage.clear()
-            navigate('/login')
+            navigate("/login")
           }
         }
           )
